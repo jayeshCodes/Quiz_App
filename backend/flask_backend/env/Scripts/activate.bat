805c04902c4b1baa3349333503c8e20968a75495
NeilJoseph251 <101554675+NeilJoseph251@users.noreply.github.com>
2023-04-28 19:32:54 +0530

    UploadImage

--- backend/flask_backend/env/Scripts/activate.bat
+++ backend/flask_backend/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Quiz_App\backend\flask_backend\env
+set VIRTUAL_ENV=C:\Users\Neil\OneDrive\Documents\GitHub\Quiz_App\backend\flask_backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ backend/flask_backend/uploadImage.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:/Users/Neil/OneDrive/Documents/GitHub/Quiz_App/backend/flask_backend/uploadImage.py'
+ALLOWED_EXTENTIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENTIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/media/upload', methods=['POST'])
+def upload_media():
+    if 'file' not in request.files:
+        return jsonify({'error': 'media not provided'}), 400
+    file - request.files['files']
+    if file.filename == '':
+        return jsonify({'error': 'no file selected'}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({'msg': 'media uploaded successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
